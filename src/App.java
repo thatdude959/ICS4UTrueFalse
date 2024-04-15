c2b2287fd324dc1e1739a4faa9b919c274fce9fa
thatdude959 <christophercheung6@gmail.com>
2024-04-15 16:48:07 -0400

    changed option chioces to True False, finished adding qestions, added restarting if user fails

--- src/App.java
+++ src/App.java
@@ -2,7 +2,6 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Boolean restart;
         int startGame = JOptionPane.showConfirmDialog(null, "Ready to play!", null, 0);
          //For some reason YES = 0 NO = 1
 
@@ -13,20 +12,33 @@ public class App {
         }
         
         while (true) {
-            restart = AskQustion("Does 1+1=2", true);
-            
+            while (true) {
+                if (AskQustion("1+1=2", 0) == false) break;
+                if (AskQustion("Apples Are A Fruit", 0) == false) break;
+                if (AskQustion("Bats are blind", 1) == false) break;
+                if (AskQustion("The blue whale is the biggest animal to have ever lived", 0) == false) break;
+                if (AskQustion("Taste buds can only be found on the tongue", 1) == false) break;
+                JOptionPane.showMessageDialog(null, "You Win!");
+                System. exit(0);
+            }
+
+            int restart = JOptionPane.showConfirmDialog(null, "Wrong answer\n Want To Retry?", null, 0);
+
+            if (restart == 1) {
+                JOptionPane.showMessageDialog(null, "Goodbye!");
+                System. exit(0);
+            }
         }
     }
-    //creating a function to ask questions and return if there correct of not
-    public static Boolean AskQustion(String message, Boolean trueFalse){
-        Boolean answer;
-        int question = JOptionPane.showOptionDialog(null, message, null, 0, 0, null, null, null);
-        if (question == 0) answer = true;
-        if (question == 1) answer = false;
+    //function to ask questions and return if there correct of not
+    public static Boolean AskQustion(String message, int trueFalse){
+        String[] options = {"True", "False"};
+        int question = JOptionPane.showOptionDialog(null, message, null, 0, 0, null, options, null);
 
-        if (answer = trueFalse) {
+        if (question == trueFalse) {
             return(true);
         }
+        
         else return(false);
     }
 }
